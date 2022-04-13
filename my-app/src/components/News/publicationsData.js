69acab1ce2a3b0e0593a251dc10cc88f103f8c31
bvbsis <mr.topal89@gmail.com>
2022-04-13 15:51:11 +0300

    add swiper and card layout

---
+++ my-app/src/components/News/publicationsData.js
@@ -0,0 +1,68 @@
+const data = [
+  {
+    img: "https://images.unsplash.com/photo-1649588519560-6ef6e2bc60ee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=432&q=80",
+    heading: "Topic-driven Ensemble for Online Advertising Generation",
+    authors:
+      "Egor Nevezhin, Nikolay Butakov, Maria Khodorchenko, Maxim Petrov, Denis Nasonov",
+    text: "Online advertising is one of the most widespread ways to reach and increase a target audience for those selling products. Usually having a form of a banner, advertising engages users into visiting a corresponding webpage. Professional generation of banners requires creative and writing skills and a basi…",
+    _id: 1,
+  },{
+    img: "https://images.unsplash.com/photo-1649588519560-6ef6e2bc60ee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=432&q=80",
+    heading: "Topic-driven Ensemble for Online Advertising Generation",
+    authors:
+      "Egor Nevezhin, Nikolay Butakov, Maria Khodorchenko, Maxim Petrov, Denis Nasonov",
+    text: "Online advertising is one of the most widespread ways to reach and increase a target audience for those selling products. Usually having a form of a banner, advertising engages users into visiting a corresponding webpage. Professional generation of banners requires creative and writing skills and a basi…",
+    _id: 2,
+  },{
+    img: "https://images.unsplash.com/photo-1649588519560-6ef6e2bc60ee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=432&q=80",
+    heading: "Topic-driven Ensemble for Online Advertising Generation",
+    authors:
+      "Egor Nevezhin, Nikolay Butakov, Maria Khodorchenko, Maxim Petrov, Denis Nasonov",
+    text: "Online advertising is one of the most widespread ways to reach and increase a target audience for those selling products. Usually having a form of a banner, advertising engages users into visiting a corresponding webpage. Professional generation of banners requires creative and writing skills and a basi…",
+    _id: 3,
+  },{
+    img: "https://images.unsplash.com/photo-1649588519560-6ef6e2bc60ee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=432&q=80",
+    heading: "Topic-driven Ensemble for Online Advertising Generation",
+    authors:
+      "Egor Nevezhin, Nikolay Butakov, Maria Khodorchenko, Maxim Petrov, Denis Nasonov",
+    text: "Online advertising is one of the most widespread ways to reach and increase a target audience for those selling products. Usually having a form of a banner, advertising engages users into visiting a corresponding webpage. Professional generation of banners requires creative and writing skills and a basi…",
+    _id: 4,
+  },{
+    img: "https://images.unsplash.com/photo-1649588519560-6ef6e2bc60ee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=432&q=80",
+    heading: "Topic-driven Ensemble for Online Advertising Generation",
+    authors:
+      "Egor Nevezhin, Nikolay Butakov, Maria Khodorchenko, Maxim Petrov, Denis Nasonov",
+    text: "Online advertising is one of the most widespread ways to reach and increase a target audience for those selling products. Usually having a form of a banner, advertising engages users into visiting a corresponding webpage. Professional generation of banners requires creative and writing skills and a basi…",
+    _id: 5,
+  },{
+    img: "https://images.unsplash.com/photo-1649588519560-6ef6e2bc60ee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=432&q=80",
+    heading: "Topic-driven Ensemble for Online Advertising Generation",
+    authors:
+      "Egor Nevezhin, Nikolay Butakov, Maria Khodorchenko, Maxim Petrov, Denis Nasonov",
+    text: "Online advertising is one of the most widespread ways to reach and increase a target audience for those selling products. Usually having a form of a banner, advertising engages users into visiting a corresponding webpage. Professional generation of banners requires creative and writing skills and a basi…",
+    _id: 6,
+  },{
+    img: "https://images.unsplash.com/photo-1649588519560-6ef6e2bc60ee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=432&q=80",
+    heading: "Topic-driven Ensemble for Online Advertising Generation",
+    authors:
+      "Egor Nevezhin, Nikolay Butakov, Maria Khodorchenko, Maxim Petrov, Denis Nasonov",
+    text: "Online advertising is one of the most widespread ways to reach and increase a target audience for those selling products. Usually having a form of a banner, advertising engages users into visiting a corresponding webpage. Professional generation of banners requires creative and writing skills and a basi…",
+    _id: 7,
+  },{
+    img: "https://images.unsplash.com/photo-1649588519560-6ef6e2bc60ee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=432&q=80",
+    heading: "Topic-driven Ensemble for Online Advertising Generation",
+    authors:
+      "Egor Nevezhin, Nikolay Butakov, Maria Khodorchenko, Maxim Petrov, Denis Nasonov",
+    text: "Online advertising is one of the most widespread ways to reach and increase a target audience for those selling products. Usually having a form of a banner, advertising engages users into visiting a corresponding webpage. Professional generation of banners requires creative and writing skills and a basi…",
+    _id: 8,
+  },{
+    img: "https://images.unsplash.com/photo-1649588519560-6ef6e2bc60ee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=432&q=80",
+    heading: "Topic-driven Ensemble for Online Advertising Generation",
+    authors:
+      "Egor Nevezhin, Nikolay Butakov, Maria Khodorchenko, Maxim Petrov, Denis Nasonov",
+    text: "Online advertising is one of the most widespread ways to reach and increase a target audience for those selling products. Usually having a form of a banner, advertising engages users into visiting a corresponding webpage. Professional generation of banners requires creative and writing skills and a basi…",
+    _id: 9,
+  },
+];
+
+export default data;
--- my-app/src/components/Publications/Publications.js
+++ my-app/src/components/Publications/Publications.js
@@ -1,11 +1,95 @@
-import Styles from "./Publications.module.css";
+import styles from "./publications.module.css";
+import "./publicationsSwiper.css";
+
+// import Swiper core and required modules
+import { Navigation, Pagination, A11y, Grid } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/grid";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import "swiper/css/a11y";
+
+import data from "../News/publicationsData";
 
 const Publications = () => {
-    return (
-        <div>
+  return (
+    <section className={styles.publications}>
+      <div className={styles.wrapper}>
+        <h2 className={styles.title}>Публикации</h2>
 
-        </div>
-    )
-}
+        <Swiper
+          wrapperTag="ul"
+          className={styles.slider}
+          modules={[Navigation, Pagination, A11y, Grid]}
+          navigation={{
+            nextEl: `.${styles.sliderButtonNext}`,
+            prevEl: `.${styles.sliderButtonPrev}`,
+          }}
+          pagination={{
+            el: `.${styles.sliderPagination}`,
+            type: "bullets",
+            bulletClass: styles.paginationBullet,
+            bulletActiveClass: styles.paginationBulletActive,
+            bulletElement: "div",
+            clickable: true,
+          }}
+          grid={{
+            rows: 2,
+            fill: "row",
+          }}
+          a11y={{
+            prevSlideMessage: "Предыдущий слайд",
+            nextSlideMessage: "Следующий слайд",
+          }}
+          spaceBetween={33}
+          slidesPerView={3}
+          slidesPerGroup={3}
+        >
+          <>
+            {data.map((el) => (
+              <SwiperSlide className={styles.card} key={el._id} tag="li">
+                <div
+                  style={{ backgroundImage: `url("${el.img}")` }}
+                  className={styles.cardImage}
+                />
+                <h3 className={styles.cardHeading}>{el.heading}</h3>
+                <span className={styles.cardAuthors}>{el.authors}</span>
+                <p className={styles.cardParagraph}>{el.text}</p>
+                <div className={styles.cardFooter}>
+                  <span className={styles.cardFooter_heading}>
+                    Читать полностью
+                  </span>
+                  <div className={styles.cardFooter_arrow} />
+                  <div className={styles.cardFooter_share} />
+                  <ul>
+                    <li>
+                      <a></a>
+                    </li>
+                    <li>
+                      <a></a>
+                    </li>
+                    <li>
+                      <a></a>
+                    </li>
+                  </ul>
+                </div>
+              </SwiperSlide>
+            ))}
+          </>
+        </Swiper>
+        <div
+          className={`${styles.sliderButtonPrev} ${styles.sliderButton}`}
+        ></div>
+        <div
+          className={`${styles.sliderButtonNext} ${styles.sliderButton}`}
+        ></div>
+        <div className={styles.sliderPagination}></div>
+      </div>
+    </section>
+  );
+};
 
 export default Publications;
